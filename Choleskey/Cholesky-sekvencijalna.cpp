0efff9342d743d97ca23e43e1a4e80e30a0430f3
etirak1 <etirak1@etf.unsa.ba>
2025-11-11 09:55:56 +0100

    Dodana poboljsana sekvencijalna implementacija i paralelizovana verzija - I verzija

---
+++ Choleskey/Cholesky-sekvencijalna.cpp
@@ -0,0 +1,159 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath> 
+#include <iomanip>
+#include <chrono>
+
+using namespace std; 
+using Matrix = vector<vector<double>>;
+
+Matrix Cholesky(const Matrix& A) {
+	size_t n = A.size();
+	const double EPS = 1e-12;
+
+	if (A.empty()) {
+		cout << "Greska: Matrica je prazna!" << endl;
+		return {};
+	}
+
+	for (const auto& row : A) {
+		if (row.size() != n) {
+			cout << "Greska: Matrica nije kvadratna!" << endl;
+			return {};
+		}
+	}
+
+
+	for (size_t i = 0; i < n; i++) {
+		for (size_t j = i + 1; j < n; j++) {
+			if (fabs(A[i][j] - A[j][i]) > EPS) {
+				cout << "Greska: Matrica nije simetricna!" << endl;
+				return {};
+			}
+		}
+	}
+
+
+	Matrix L(n, vector<double>(n, 0.0));
+	for (size_t i = 0; i < n; i++) {
+		for (size_t j = 0; j <= i; j++) {
+			double suma = 0.0;
+			for (size_t k = 0; k < j; k++)
+				suma += L[i][k] * L[j][k];
+			if (i == j) {
+				double val = A[i][i] - suma;
+				if (val <= 0) {
+					cout << "Matrica nije pozitivno definitna!\n";
+					exit(1);
+				}
+				L[i][j] = sqrt(val);
+			}
+			else {
+				L[i][j] = (A[i][j] - suma) / L[j][j];
+			}
+		}
+	}
+
+	return L;
+}
+
+
+Matrix generisiPozitivnoDefinitnu(size_t n) {
+	Matrix A(n, vector<double>(n));
+
+	for (size_t i = 0; i < n; i++) {
+		for (size_t j = i; j < n; j++) {
+			double val = (double)rand() / RAND_MAX;
+			A[i][j] = A[j][i] = val;
+		}
+	}
+
+	Matrix C(n, vector<double>(n, 0.0));
+	for (size_t i = 0; i < n; i++)
+		for (size_t j = 0; j < n; j++)
+			for (size_t k = 0; k < n; k++)
+				C[i][j] += A[i][k] * A[j][k];
+
+	return C;
+}
+
+	/*
+	cout << "L = " << endl;
+	for (size_t i = 0; i < n; i++) { 
+		for (size_t j = 0; j < n; j++)
+			cout << setw(10) << fixed << setprecision(4) << L[i][j] << " ";
+		cout << endl;
+	}
+*/
+/*	cout << "\n Provjera: L * L^T = " << endl;
+	Matrix check(n, vector<double>(n, 0.0));
+	for (size_t i = 0; i < n; i++){
+		for (size_t j = 0; j < n; j++){ 
+			for (size_t k = 0; k < n; k++)
+				check[i][j] += L[i][k] * L[j][k];
+		}
+	}
+
+	for (size_t i = 0; i < n; i++) 
+	{ 
+		for (size_t j = 0; j < n; j++)
+			cout << setw(10) << fixed << setprecision(4) << check[i][j] << " ";
+		cout << endl;
+	}
+	return L;
+}
+
+*/
+
+int main() {
+/*	cout << "Test 1: Ispravna matrica\n" << endl;
+	Matrix A = { { 4, 12, -16 }, { 12, 37, -43 }, { -16, -43, 98 } };
+	Cholesky(A); 
+
+
+	auto start = chrono::high_resolution_clock::now();
+	auto L = Cholesky(A);
+	auto end = chrono::high_resolution_clock::now();
+
+	auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+	cout << "Vrijeme izvrsavanja: " << duration.count() << " ms" << endl;
+
+
+	cout << "\n Test 2: Prazna matrica \n" << endl;
+	Matrix B = {};
+	Cholesky(B);
+
+	cout << "\n Test 3: Matrica nije kvadratna \n " << endl;
+	Matrix C = { {4, 12}, {-16, 12}, {37, -43} };
+	Cholesky (C);
+
+	cout << "\n Test 4: Matrica nije simetricna \n " << endl;
+	Matrix D = { { 1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+	Cholesky(D);
+
+	cout << "\n Test 5: Matrica nije pozitivno definitna \n" << endl;
+	Matrix E = { {1, 0}, {0, -1} };
+	Cholesky(E);
+
+	*/
+
+
+	srand(time(0));
+
+	vector<int> velicine = { 500, 1000, 2000, 3000, 5000, 6000 };
+
+	for (int n : velicine) {
+		cout << "\n Test za matricu " << n << "x" << n << endl;
+		Matrix A = generisiPozitivnoDefinitnu(n);
+
+		auto start = chrono::high_resolution_clock::now();
+		Matrix L = Cholesky(A);
+		auto end = chrono::high_resolution_clock::now();
+
+		auto trajanje = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+		cout << "Vrijeme izvrsavanja: " << trajanje << " ms" << endl;
+	}
+	
+
+	return 0;
+}
---
+++ Choleskey/CholeskySIMD.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath> 
+#include <iomanip>
+#include <chrono>
+#include <omp.h>
+
+using namespace std;
+using Matrix = vector<vector<double>>;
+
+Matrix Cholesky(const Matrix &A) {
+    size_t n = A.size();
+    Matrix L(n, vector<double>(n, 0.0));
+
+    omp_set_num_threads(2);
+
+    for (size_t i = 0; i < n; i++) {
+        double suma = 0.0;
+#pragma omp simd reduction(+:suma)
+        for (size_t k = 0; k < i; k++)
+            suma += L[i][k] * L[i][k];
+        double val = A[i][i] - suma;
+        if (val <= 0) {
+            cout << "Matrica nije pozitivno definitna!" << endl;
+            exit(1);
+        }
+        L[i][i] = sqrt(val);
+
+#pragma omp parallel for schedule(static)
+        for (int j = i + 1; j < (int)n; j++) {
+            double suma2 = 0.0;
+#pragma omp simd reduction(+:suma2)
+            for (size_t k = 0; k < i; k++)
+                suma2 += L[j][k] * L[i][k];
+            L[j][i] = (A[j][i] - suma2) / L[i][i];
+        }
+    }
+
+    return L;
+}
+
+Matrix generisiPozitivnoDefinitnu(size_t n) {
+    Matrix A(n, vector<double>(n));
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = i; j < n; j++) {
+            double val = (double)rand() / RAND_MAX;
+            A[i][j] = A[j][i] = val;
+        }
+        A[i][i] += n; 
+    }
+    return A;
+}
+
+
+int main() {
+    srand(0); 
+    omp_set_num_threads(2);
+
+    vector<int> velicine = { 500, 1000, 2000, 3000, 5000, 6000 };
+
+    cout << "Broj niti (jezgra): " << omp_get_max_threads() << endl;
+
+    for (int n : velicine) {
+        cout << "\nTest za matricu " << n << "x" << n << endl;
+        Matrix A = generisiPozitivnoDefinitnu(n);
+
+        int ponavljanja = 5;
+        long long ukupno = 0;
+
+        for (int p = 0; p < ponavljanja; p++) {
+            auto start = chrono::high_resolution_clock::now();
+            Matrix L = Cholesky(A);
+            auto end = chrono::high_resolution_clock::now();
+            ukupno += chrono::duration_cast<chrono::milliseconds>(end - start).count();
+        }
+
+        cout << "Prosjecno vrijeme izvrsavanja: " << ukupno / ponavljanja << " ms" << endl;
+    }
+
+    return 0;
+}
